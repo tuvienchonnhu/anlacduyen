53adb983837eec80c0c8500daaceb6f2fad02aea
tuvienchonnhu <tuvienchonnhu1928.10@gmail.com>
2026-09-14 08:18:55 +0700

    không hiển thị thuộc tính của ảnh, mất thuộc tính img

--- _policytest/PolicyTest.java
+++ _policytest/PolicyTest.java
@@ -4,10 +4,10 @@ public class PolicyTest {
   public static void main(String[] a) throws Exception {
     InputStream in = new FileInputStream("C:/web/shopizer-2.17.0/sm-shop/target/classes/antisamy-slashdot.xml");
     Policy p = Policy.getInstance(in);
-    String html = "<p>Hello <img src=\"/static/img/test.png\" alt=\"anh\" width=\"200\"/> <span class=\"x\">span</span> <table><tr><td colspan=\"2\">cell</td></tr></table></p>";
+    String html = "<img src=\"/static/img/test.png\" alt=\"anh\" class=\"img-responsive\" width=\"200\" style=\"margin: 10px 20px; border-width: 10px; border-style: solid; float: left;\"/>";
     AntiSamy as = new AntiSamy();
     CleanResults cr = as.scan(html, p);
-    System.out.println("POLICY OK - cleaned output:");
+    System.out.println("OUTPUT:");
     System.out.println(cr.getCleanHTML());
   }
 }
